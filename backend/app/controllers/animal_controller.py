83633950f9ddf9b1d0c0e2531e2747ea6431d1e3
Teshyo <guyman12330@gmail.com>
2026-02-28 17:43:58 +0200

    feat/filter-animals-endpoint (#35)

--- backend/app/controllers/animal_controller.py
+++ backend/app/controllers/animal_controller.py
@@ -1,18 +1,36 @@
 # Animal controller - business logic for animal CRUD operations
 from app.models.animal import Animal
+from datetime import datetime,  timedelta
+from flask import abort
 
-def get_all_animals():
+def get_animals(args):
     """
-    Get all animals from db
+    Get all animals that meet selected filters
+    Get all animals from db if no filters selected
     Returns list of animal dictionaries
     """
-    animals = Animal.query.all()
-    return [animal.to_dict() for animal in animals]
+    # By which parameters animal entries can be filtered by
+    # created_at is not here because it needs special actions
+    regular_filters = {
+        'id', 'name', 'type', 'breed', 'size', 'age', 
+        'vaccinated', 'tem  perament', 'description', 
+        'adopted'
+    }
 
-def get_animal_by_id(animal_id):
-    """
-    Get a single animal by ID
-    Returns animal dictionary or None if not found
-    """
-    animal = Animal.query.get(animal_id)
-    return animal.to_dict() if animal else None
+    filter_data = {key: value for key, value in args.items() if key in regular_filters}
+
+    query = Animal.query.filter_by(**filter_data)
+
+    # created_at special actions
+    if 'created_at' in args:
+        try:
+            date_value = args['created_at']
+            start_date = datetime.strptime(date_value, "%Y-%m-%d")
+            end_date = start_date + timedelta(days=1)
+
+            query = query.filter(Animal.created_at >= start_date, Animal.created_at < end_date)
+        except ValueError:
+            abort(400, description="Invalid date format, use YYYY-MM-DD")
+    
+    animals = query.all()
+    return [animal.to_dict() for animal in animals]
--- backend/app/routes/animal_routes.py
+++ backend/app/routes/animal_routes.py
@@ -1,21 +1,11 @@
 # Animal routes - Flask blueprint defining API endpoints for /api/animals
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from app.controllers import animal_controller
 
 animal_bp = Blueprint('animals', __name__)
 
 @animal_bp.route('', methods=['GET'])
 def get_animals():
-    # GET /api/animals - get all animals
-    animals_list = animal_controller.get_all_animals()
+    # get list of animals based on arguments in args
+    animals_list = animal_controller.get_animals(request.args)
     return jsonify({"animals": animals_list}), 200
-
-@animal_bp.route('/<int:animal_id>', methods=['GET'])
-def get_animal(animal_id):
-    # GET /api/animals/<id> - get a single animal by ID
-    animal = animal_controller.get_animal_by_id(animal_id)
-    
-    if animal is None:
-        return jsonify({'error': 'Animal not found'}), 404
-    
-    return jsonify(animal), 200
