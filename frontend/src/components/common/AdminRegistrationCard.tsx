f137ac190b36f495575289143e539258d06b23fb
Hash sop <guyman12330@gmail.com>
2026-03-22 23:12:23 +0200

    feat/volunteering confirmation

--- frontend/src/components/common/AdminRegistrationCard.tsx
+++ frontend/src/components/common/AdminRegistrationCard.tsx
@@ -56,6 +56,7 @@ function AdminRegistrationCard({
       onUpdate(updatedData);
     } catch (error) {
       alert("Failed to approve registration");
+      console.error("Error approving registration", error)
     }
   };
 
@@ -65,6 +66,7 @@ function AdminRegistrationCard({
       onUpdate(updatedData);
     } catch (error) {
       alert("Failed to mark registration attendance");
+      console.error("Error marking attendance:", error)
     }
   };
 
